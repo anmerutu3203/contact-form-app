f6ae321720b43bef699c32cc2ed5f4a658b9b0b1
anmerutu3203 <anmerutu3203@gmail.com>
2026-08-26 21:13:59 +0900

    feat:お問い合わせフォーム確認ページ実装#5

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,8 @@
 <?php
 
 use App\Http\Controllers\ContactController;
+use Illuminate\Support\Facades\Route;
+
 
 Route::get('/', [ContactController::class, 'index'])->name('contact.index');
+Route::post('/contacts/confirm', [ContactController::class, 'confirm'])->name('contact.confirm');
